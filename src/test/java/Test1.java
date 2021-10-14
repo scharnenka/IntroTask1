fcb9d323ec304633216dea5d351164d81f3c20f7
Stanislau_Charnenka <Stanislau_Charnenka@epam.com>
2021-10-14 17:14:47 +0300

    Implement DI for using web driver

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,11 +1,22 @@
+import com.google.inject.Inject;
+import driverManger.DriverModule;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Guice;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+@Guice(modules = {
+        DriverModule.class
+})
 public class Test1 {
+
+    @Inject
+    WebDriver driver;
 
     @Parameters({"data1a_1"})
     @Test
     public void action1_a(String data1a_1) {
+        driver.get("https://www.google.com/");
         System.out.println("Test1->action1_a->data1a_1");
     }
 
--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,11 +1,22 @@
+import com.google.inject.Inject;
+import driverManger.DriverModule;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Guice;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+@Guice(modules = {
+        DriverModule.class
+})
 public class Test2 {
+
+    @Inject
+    WebDriver driver;
 
     @Parameters({"data2a_1"})
     @Test
     public void action2_a(String data2a_1) {
+        driver.get("https://yandex.by/");
         System.out.println("Test2->action2_a->data2a_1");
     }
 
